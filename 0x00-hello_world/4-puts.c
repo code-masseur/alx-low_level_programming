08e07409a8d373905674ac404f9d345778a17b4e
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-21 17:04:57 -0700

    A C program that prints a statement to the standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * Program starts from main
+ *
+ * Return value is 0 if everything runs accordingly
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
