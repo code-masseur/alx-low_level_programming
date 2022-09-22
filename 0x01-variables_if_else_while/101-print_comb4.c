9614e6fa39eee0bff1e4f8f300f186c83fbd1ee0
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-22 11:45:25 -0700

    Fixed betty style issue

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,29 +10,17 @@ int main(void)
 	int i, e, g;
 
 
-
 	i = 48;
-
 	e = 48;
-
 	g = 48;
 
-
-
 	while (e < 58)
-
 	{
-
 		i = 48;
-
 		while (i < 58)
-
 		{
-
 			g = 48;
-
 			while (g < 58)
-
 			{
 				if (e != i && e != g && i != g && e < i && i < g)
 				{
@@ -45,13 +33,9 @@ int main(void)
 					}
 					putchar(',');
 					putchar(' ');
-
 				}
-
 				g++;
-
 			}
-
 			i++;
 		}
 		e++;
