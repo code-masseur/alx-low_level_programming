e10ef2094a3c429e9279162e77169d19f1767e32
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-22 11:14:17 -0700

    Fix lines error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,43 +10,27 @@ int main(void)
 	int i, e;
 
 
-
 	i = 48;
-
 	e = 48;
-
-
-
 	while (e < 58)
 	{
 		i = 48;
-
 		while (i < 58)
-
 		{
-
 			if (e != i && e < i)
-
 			{
-
 				putchar(e);
-
 				putchar(i);
-
 				if (i == 57 && e == 56)
-
 				{
-
 					break;
 
 				}
 				putchar(',');
 				putchar(' ');
-
 			}
 
 			i++;
-
 		}
 
 		e++;
