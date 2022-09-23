d565ac9361431d57ff8746a0b8b82dcc26b80cc9
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-23 10:24:51 -0700

    Function checks for alphabetic char and return 1 if true, 0 if other case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character,
+ *
+ * @c: is the character in ANSI code
+ *
+ * Return: return 1 if true, 0 if other case
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97; c <= 122) || (c >= 65; c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
