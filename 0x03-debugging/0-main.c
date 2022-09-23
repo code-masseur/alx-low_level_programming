1f4383055526fa1e5a85ea8d986a978d3ac905a5
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-22 20:12:56 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include<stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative 
 * Return: 0
@@ -9,6 +9,5 @@ int main(void)
 	
         i = 0;
         positive_or_negative(i);
-
         return (0);
 }
