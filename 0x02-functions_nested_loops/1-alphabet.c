2d698b1c63809911dee26d7d1b8cc8b5a39758c1
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-23 04:52:35 -0700

    Print alphabet in lowercase 10 times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints alphabets in lowercase
+ * print_alphabet - prints alphabets in lowercase
  * Return: always 0 (success)
  */
 void print_alphabet(void) /* this function gets all lowercase alphabets*/
