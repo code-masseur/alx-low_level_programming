c30becb1486c38020b0936f01e8fce5ed107c912
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-09-22 19:00:00 -0700

    fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,7 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
-	}
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
