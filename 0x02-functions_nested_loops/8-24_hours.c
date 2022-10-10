0be0f4afeecd71f099fc3a8d18cc6c19f988b9fb
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-09 19:09:25 -0700

    Jack Bauer - 24 Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer: prints every minute of the day
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+
+	int i;
+	int j;
+	int k;
+	int l;
+	int m;
+	int n;
+
+	for (i = 0; i < 24; i++)
+	{
+		j = i / 10;
+		k = i % 10;
+	
+		for (l = 0; l < 60; l++)
+		{	
+			m = l / 10;
+			n = l % 10;
+			_putchar('0' + j);
+			_putchar('0' + k);
+			_putchar(':');
+			_putchar('0' + m);
+			_putchar('0' + n);
+			_putchar('\n');
+		}
+	}
