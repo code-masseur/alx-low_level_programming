ca461af70ec8093a7cd3023adccd2ee2654f45b4
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-13 10:30:59 -0700

    Function to check for a digit using ASCII values

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks from 0-9 to see if it's a digit
+ * @c: An automatic integer variable to be used by the function
+ *
+ * Return: returns 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int result;
+
+	if (c >= 48 && c <= 57)
+	{
+		result = 1;
+		return (result);
+	}
+	else
+	{
+		result = 0;
+		return (result);
+	}
+}
