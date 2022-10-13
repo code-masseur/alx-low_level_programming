a743490007859d3ac61ff0d0308799b1c1580c5a
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-13 10:44:36 -0700

    Function to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first auto variable to hold the first number
+ * @b: second auto variable to hold the second number
+ *
+ * Return: returns the result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
