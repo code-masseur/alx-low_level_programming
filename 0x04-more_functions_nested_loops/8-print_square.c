bcf7ff60945dc7355199c29074a6eec37a11234b
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-13 16:10:37 -0700

    Function to print square shape to the screen

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/** print_square - prints square to the screen
+ * @size: variable to determine the size of the square in the loop
+ *
+ * Return: return void
+ */
+
+void print_square(int size)
+{
+	int y;
+	int z;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (z = 0; z < y; z++)
+			{
+				putchar(35);
+			}
+			putchar('\n');
+		}
+	}
+}
