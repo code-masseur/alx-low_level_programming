e3bc8563d403d298ab2e29665a4cb063212b4806
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-13 18:08:12 -0700

    Function to print triangular shape

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a traingular shape to the output stream
+ * @size: variable that determines the size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{	
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				putchar(32);
+			}
+		
+			for (c = 0; c <= a; c++)
+			{
+				putchar(35);
+			}
+			putchar('\n');
+		}
+	}
+}
