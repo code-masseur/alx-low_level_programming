d1edc9e859bc02fafd8d16746a4e45d81c08af91
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-13 17:35:21 -0700

    Applied a fix to the FizzBuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int main(void)
 
 	for (z = 1; z <= 100; z++)
 	{
-		if (z % 3 == 0)
+		if ((z % 3 == 0) && (z % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (z % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (z % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (z % 3 == 0 && z % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
@@ -34,7 +34,7 @@ int main(void)
 			putchar(32);
 		}
 	}
-	printf('\n');
+	putchar('\n');
 
 	return (0);
 }
