094b12ea77a80a33bf2ee4d7bd7e16ff4687bcb4
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 15:33:50 -0700

    Fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - adds new node to beginning of linked list
- * @head: input head of linked list
- * @n: new number to go into new node
- * Return: address of new node or NULL
- */
+ * add_nodeint - Add a new node at the beginning of a list.
+ * @head: Address of the first node of a list.
+ * @n: Integer to insert into the new node.
+ * Return: Address of the new node.
+ **/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	listint_t *tp;
+	tp = malloc(sizeof(listint_t));
+	if (tp == NULL)
 		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	tp->n = n;
+	tp->next = *head;
+	*head = tp;
+	return (*head);
 }
