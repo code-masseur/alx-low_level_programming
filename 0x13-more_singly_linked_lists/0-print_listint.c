54e4a1d3fd350958eb7d6de0f1328a035de06830
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 15:42:42 -0700

    Fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,6 +9,7 @@ size_t print_listint(const listint_t *h)
 {
 	unsigned int i = 0;
 	const listint_t *temp_h = h;
+
 	while (temp_h)
 	{
 		printf("%d\n", temp_h->n);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,6 +9,7 @@ size_t listint_len(const listint_t *h)
 {
 	unsigned int i = 0;
 	const listint_t *temp_h = h;
+
 	for (; temp_h; temp_h = temp_h->next)
 		i++;
 	return (i);
