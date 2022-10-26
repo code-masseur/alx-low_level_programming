d0fb4d81c104b00709d58abc188638e5ae09274a
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 20:04:20 -0700

    This program writes a 64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints hello holberton to the output stream
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", "Hello, Holberton");
+
+	return (0);
+}
