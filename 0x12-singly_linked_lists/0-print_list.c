a6610cee3b0b9c0341724350b3f4047ba66a962e
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 18:36:23 -0700

    This is a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,17 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * print_list - prints elements of linked list
+ * @h: name of linked list input
+ * Return: number of nodes printed
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	unsigned int i = 0;
+	const list_t *temp_h = h;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
+	for (; temp_h; temp_h = temp_h->next, i++)
+		printf("[%d] %s\n", temp_h->len, temp_h->str);
+	return (i);
 }
