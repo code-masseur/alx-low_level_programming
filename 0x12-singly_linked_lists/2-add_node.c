ca1d9c02a6897a5ac76072113586404a7174267f
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 19:07:35 -0700

    Fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
- * Return: If the function fails - NULL, Otherwise - the address of the new element.
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * add_node - adds new node to beginning of linked list
+ * @head: beginning of linked list
+ * @str: new string to be added to link list
+ * Return: pointer to new node
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_node;
 
-	char *dup_str;
-	int len;
+	new_node = malloc(sizeof(list_t));
 
-	new = malloc(sizeof(list_t));
-
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-	dup_str = strdup(str);
-
-	if (dup_str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup_str;
-	new->len = len;
-	new->next = *head;
-	*head = new;
-
-	return (new);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
