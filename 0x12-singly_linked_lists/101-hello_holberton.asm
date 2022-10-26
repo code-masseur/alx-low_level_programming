d97e3fd4a734aab3b933fa578eb22688176ce6f4
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 20:42:17 -0700

    Fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,44 @@
 	.file	"101-hello_holberton.c"
+	.text
 	.section	.rodata
-CLI
 .LC0:
-	.string	"Hello, Bootcamp"
+	.string	"Hello, Holberton"
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	pushq	%rcx
+	endbr64
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rax, %rcx
+	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
 	movl	$0, %eax
-	popq	%rcx
+	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
