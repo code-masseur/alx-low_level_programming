39097c988ea32881de95eb98302440684b19ff9b
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 21:21:39 -0700

    Fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,88 +1,35 @@
 #include "lists.h"
 
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - Counts the number of unique nodes in a looped list * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0, Otherwise - the number of unique
- * nodes on the lists
- */
-
-size_t looped_listint_count(listint_t *head)
-{
-	listint_t *tortoise, *hare;
-
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-			return (nodes);
-		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-	return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely
- * @h: A pointer to the address of the head of the listint_t list.
- * Return: The size of the list that was freed.
- * Description: The function sets the head to NULL.
+ * free_listint_safe - frees linked list safe version
+ * @h: input head of linked list
+ * Return: size of list that was freed
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp;
+	size_t i = 0, j;
+	listint_t *temp, *check, *copy = *h;
 
-	size_t nodes, index;
-
-	nodes = looped_listint_count(*h);
-
-	if (nodes == 0)
+	while (*h)
 	{
-		for (; h != NULL && *h != NULL; nodes++)
+		i++;
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		check = copy;
+		j = 0;
+
+		while (j < i)
 		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
+			if (*h == check)
+			{
+				*h = NULL;
+				return (i);
+			}
+			check = check->next;
+			j++;
 		}
 	}
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-		*h = NULL;
-	}
-	h = NULL;
-	return (nodes);
+	return (i);
 }
