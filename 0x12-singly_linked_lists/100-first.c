94f2961e5cbc5020ee43642ea1a5fe0958c1895a
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-25 19:31:01 -0700

    Fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * before_main - function executed before main()
+ * call_before_main - function executed before main()
  * Return: void
  */
 
-void before_main(void)
+void call_before_main(void)
 {
 	printf("%s\n", "You're beat! and yet, you must allow,");
 	printf("%s\n", "I bore my house upon my back!");
