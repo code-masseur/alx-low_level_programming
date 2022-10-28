54883b2db276cca027e6f35c76c590bd13758f5c
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-10-27 19:13:04 -0700

    Fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,9 +2,11 @@
 
 /**
  * binary_to_uint - converts binary to unsigned int
- * @b: binary string
- * Return: decimal # or 0 if fails
+ * @b: character pointer to the string
+ * Return: If b is NULL or contains chars not 0 or 1,
+ * otherwise the converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, decimal = 0;
