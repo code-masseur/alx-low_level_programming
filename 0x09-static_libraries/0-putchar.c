830d27b99a2accf0c24d3965cb83dfc56f0c9d96
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-11-13 22:25:04 -0800

    I added even more object files to the static library 'libmy.a'

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,21 +1,6 @@
-#include<stdio.h>
 #include "main.h"
 
-/**
-* main - prints _putchar to the stdout
-* Return: Always 0 (success)
-*/
-int main(void)
+int _putchar(__attribute__((unused))char c)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-
-	for (count = 0; count < sz; count++)
-	{
-		putchar(str[count]);
-	}
-	putchar('\n');
 	return (0);
 }
