2becd35b6bcc15f8dcb4ac944db9838503356dd7
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-11-13 20:08:38 -0800

    i added more files to the static library 'libmy.a'

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if the input is lowercase
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 if input is lowercase, and 0 is other case is true
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
