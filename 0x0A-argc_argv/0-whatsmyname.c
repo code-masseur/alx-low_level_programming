7bdbbe3d4f5490d027c93c3c929bafdd3e30b6bf
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-11-23 15:50:13 -0800

    This program prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - prints its name, prints the new name if renamed anytime 
+ * without re-compilling the program
+ *
+ * Return: returns 0 (if success)
+ *
+ */
+
+int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
