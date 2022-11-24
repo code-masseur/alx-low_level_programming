c4b15f4a3cacfa147b4d430925f518833a62ce50
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-11-23 16:36:43 -0800

    This program prints the number of args passed to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main prints the number of arguments passed to it
+ * @argc: parameter to hold the argument count to main
+ * @argv: parameter to hold the name of the argument to main
+ *
+ * Return: returns 0 (if success)
+ *
+ */
+
+int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
