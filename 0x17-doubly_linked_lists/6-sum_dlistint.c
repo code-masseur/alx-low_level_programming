69f1326e8909aefe0e8779b3ca30304a5cfe1be6
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-08 15:13:08 -0800

    This function returns the sum of all the data (n) of a dlistint_t linked list.

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *sum_dlistint - sums all values of each element of the doubly link list
+ *@head: double liked list
+ *Return: int value of the sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (sum);
+
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
