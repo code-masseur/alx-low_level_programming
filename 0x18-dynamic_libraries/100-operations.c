0dbd6cf71b82f3550da9c81c45b67a16e29f1a17
Jude Maduabuchi <github.icontrol@simplelogin.com>
2022-12-12 19:10:48 -0800

    I created a dynamic library that contains C functions, then imported the C dynamic lib in my Python program for use. Pretty cool

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of the two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides an integer by anotjjer integer
+ * @a: integer
+ * @b: divisor
+ * Return: the quotient of the two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
